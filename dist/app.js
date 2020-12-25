cce58080d3ee9a36c5c574707bf04a40bc28bf09
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2020-12-25 21:49:41 +0300

    The rebuild process has been performed in the project for changes.

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,3 @@
-"use strict";
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _service = require('./lib/service'); var _service2 = _interopRequireDefault(_service);
+
+_service2.default.call(void 0, 2);
--- dist/lib/service.js
+++ dist/lib/service.js
@@ -1,10 +1,10 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});const axios = require('axios');
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _axios = require('axios'); var _axios2 = _interopRequireDefault(_axios);
 
 const endpoint = 'https://jsonplaceholder.typicode.com/';
 
 async function getData(userId) {
-    const { data: user } = await axios.get(`${endpoint}users/${userId}`)
-    const { data: posts } = await axios.get(`${endpoint}posts?userId=${userId}`)
+    const { data: user } = await _axios2.default.get(`${endpoint}users/${userId}`)
+    const { data: posts } = await _axios2.default.get(`${endpoint}posts?userId=${userId}`)
 
     const emp = { ...user, posts };
     console.log(emp);
